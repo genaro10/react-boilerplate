1e28053f0ebcfa9f8031da20cd791a643cff059b
Genaro Lorio <genarolorio10@gmail.com>
2022-12-28 18:03:48 -0300

    dale

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactssdssd
+          Learn Reactssdssdds
         </a>
       </header>
     </div>
