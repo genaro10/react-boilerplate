4b72ab68421f69f44487639c4235394072a9e27e
Genaro Lorio <genarolorio10@gmail.com>
2022-12-28 18:10:04 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactssdssdds
+          Learn Reactssdssddsasdasdsa
         </a>
       </header>
     </div>
